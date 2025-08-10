62b72900821265985b4c193847469c4a4041c35b
iamyuchy <iamyuchy@gmail.com>
2025-08-09 21:23:06 -0700

    Initial implementation for magnifier

---
+++ Assets/Script/DragMagnifyingGlass.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:22fd5dc600a80c60fc7edf31d9eea6297895f7faa93cb99ffe83bba66f29bd11
+size 1420
---
+++ Assets/Script/Lens.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:71b3c77cb1d26844f72b0ce2c4329b1b9858f4575b12773921e28d8cbc6678fe
+size 289
